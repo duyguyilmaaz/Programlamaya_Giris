ac4cdfd637e8c68c9d34b16558251d500b8a9e78
Duygu Yılmaz <duygu.yilmaz2@ogr.gelisim.edu.tr>
2024-10-24 18:29:08 +0300

    Add files via upload

---
+++ ders1.py
@@ -0,0 +1,136 @@
+# print("Hello World")
+# if (5>2):
+#     print("Beş ikiden büyüktür.")
+x = 5
+y = "Hello world"
+print(x)
+print(y)
+"""
+Çok 
+satırlı 
+yorum 
+satırı.
+"""
+# x = 5         #int
+# y = "Duygu"   #str
+# x = str(3)      #"3"
+# y = int(3)      #3
+# z = float(3)    #3.0
+# x = "Duygu"
+# y = 20
+# print(type(x))
+# print(type(y))
+# x = "Duygu" # "" ile '' aynıdır
+# x = 'Duygu'
+a = 5
+A = 15
+print(a)
+print(A) #Büyük harf ile küçük harf duyarlılığı
+# Geçerli Değişken Adları
+myvar = "Duygu"
+my_var = "Duygu"
+_my_var = "Duygu"
+myVar = "Duygu"
+MYVAR = "Duygu"
+myvar2 = "Duygu"
+# Geçersiz Değişken Adları
+# 2myvar = "Duygu"
+# my-var = "Duygu"
+# my var = "Duygu"
+# Çok Kelimeli Değişken İsimleri
+myVariableName = "Duygu"     # Deve Kılıfı
+MyVariableName = "Duygu"     # Pascal Kılıf
+my_variable_name = "Duygu"   # Yılan Kılıfı
+# Birden Çok Değişkene Birçok Değer Atama
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+# Birden Çok Değişkene Bir Değer Atama
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+# Bir Koleksiyonu Paketinden Çıkarma
+# fruits = ["Apple", "Banana", "Cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+# Çıktı Değişkenleri
+# x = "Python"
+# y = "is"
+# z = "avesome"
+# print(x, y, z)
+# print(x +" "+ y +" "+ z)
+# x = 5
+# y = 10
+# print(x + y)
+# x = 5
+# y = "Duygu"
+# print(x + y) #Hata verir.
+# x = 5
+# y = "Duygu"
+# print(x, y)
+# Global Değişkenler
+# x = "awesome"
+# def myfunc():
+#     print("Python is " + x)
+# myfunc()
+# x = "awesome"
+# def myfunc():
+#     x = "fantastic"
+#     print("Python is " + x)
+# myfunc()
+# print("Python is " + x)
+# Global Anahtar Kelime
+# def myfunc():
+#     global x
+#     x = "fantastic"
+# myfunc()
+# print("Python is " + x)
+# x = "awesome"
+# def myfunc():
+#     global x
+#     x = "fantastic"
+# myfunc()
+# print("Python is " + x)
+# Yerleşik Veri Türleri
+# Metin Türü: str
+# Sayısal Türler : int, , float, complex
+# Sıra Türleri : list, , tuple, range
+# Eşleşme Türü : dict
+# Set Türleri : set, frozenset
+# Boole Türü : bool
+# İkili Türler : bytes, , bytearray, memoryview
+# Hiçbiri Türü : NoneType
+x = "Hello World!"        #str
+x = 55                    #int
+x = 55.5                  #float
+x = 1j                    #complex
+x = ["banana", "apple", "cherry"]     #list
+x = ("banana", "apple", "cherry")     #tuple
+x = range(6)                          #range
+x = {"name" : "Duygu", "age": 20}     #dict
+x = {"banana", "apple", "cherry"}     #set
+x = frozenset({"apple", "banana", "cherry"})  #frozenset
+x = True                                      #bool
+x = b"Hello"                                  #bytes
+x = bytearray(5)                              #bytearray
+x = memoryview(bytes(5))                      #memoryview
+x = None                                      #NoneType
+# Belirli Veri Türünü Ayarlama
+x = str("Hello World")
+x = int(20)
+x = float(20.5)
+x = complex(1j)
+x = list(("banana", "apple", "cherry"))
+x = tuple(("banana", "apple", "cherry"))
+x = range(6)
+x = dict(name = "Duygu", age = 20)
+x = set(("banana", "apple", "cherry"))
+x = frozenset(("banana", "cherry", "apple"))    
+x = bool(5)
+x = bytes(5)
+x = bytearray(5)
+x = memoryview(bytes(5))
